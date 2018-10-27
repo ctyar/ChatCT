dabb8b4be96b663e47ae351b863aaaa8d31b64b3
Shahriar Gholami <shrgholami@gmail.com>
2018-10-27 12:33:53 +0330

    Implement a basic producer consumer

--- ChatCT/MainWindow.xaml.cs
+++ ChatCT/MainWindow.xaml.cs
@@ -1,28 +1,93 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Collections.Concurrent;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace ChatCT
+namespace ChatCT.Wpf
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private CancellationTokenSource _cancellationTokenSource;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            Start.Click += StartOnClick;
+            Stop.Click += StopOnClick;
+        }
+
+        private void StopOnClick(object sender, RoutedEventArgs e)
+        {
+            _cancellationTokenSource?.Cancel();
+        }
+
+        private async void StartOnClick(object sender, RoutedEventArgs e)
+        {
+            _cancellationTokenSource = _cancellationTokenSource ?? new CancellationTokenSource();
+            var blockingCollection = new BlockingCollection<string>();
+            var producer = new Producer(blockingCollection, _cancellationTokenSource.Token);
+
+            var producerTask = Task.Run(() => producer.GetMessagesAsync());
+
+            while (!blockingCollection.IsCompleted)
+            {
+                try
+                {
+                    if (blockingCollection.TryTake(out var message, 0, _cancellationTokenSource.Token))
+                    {
+                        AddMessage(message);
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    break;
+                }
+
+                await Task.Delay(10);
+            }
+
+            _cancellationTokenSource.Dispose();
+            _cancellationTokenSource = null;
+        }
+
+        private void AddMessage(string message)
+        {
+            var stackPanel = new StackPanel();
+            stackPanel.Orientation = Orientation.Horizontal;
+            stackPanel.Children.Add(GetStringMessage(message));
+            stackPanel.Children.Add(GetEmoji());
+            stackPanel.Children.Add(GetStringMessage("Some more text."));
+
+            var blockUiContainer = new BlockUIContainer();
+            blockUiContainer.Child = stackPanel;
+
+            ChatBox.Blocks.Add(blockUiContainer);
+        }
+
+        private UIElement GetStringMessage(string message)
+        {
+            var label = new Label { Content = message };
+
+            return label;
+        }
+
+        private UIElement GetEmoji()
+        {
+            var image = new Image
+            {
+                Source = new BitmapImage(new Uri(@"C:\Users\Shahriar\Pictures\1.jpg", UriKind.Absolute)),
+                Height = 20
+            };
+
+            return image;
         }
     }
 }
